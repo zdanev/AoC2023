5226c532929ff5951c3438aa68a0820c8ef15c13
Zdravko Danev <zdanev@gmail.com>
2023-11-30 22:10:17 -0800

    day 1

--- day01/part1.py
+++ day01/part1.py
@@ -1,21 +1,10 @@
 lines = None
 with open('day01/input.txt') as f:
-    lines = f.readlines()
+    lines = map(str.strip, f.readlines())
 
 s = 0
 for line in lines:
-    line = line.strip()
-
-    a = ""
-    b = ""
-    for c in line:
-        if c.isnumeric(): 
-            a = c
-            break
-    for c in line[::-1]:
-        if c.isnumeric(): 
-            b = c
-            break
-    s = s + int(a+b)
+    q = list(filter(str.isdigit, list(line)))
+    s += int(q[0] + q[-1])
 
 print(s)
--- day01/part2.py
+++ day01/part2.py
@@ -1,31 +1,16 @@
 lines = None
 with open('day01/input.txt') as f:
-    lines = f.readlines()
+    lines = map(str.strip, f.readlines())
+
+numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 s = 0
 for line in lines:
-    line = line.strip()
 
-    line = line.replace("one", "one1one")
-    line = line.replace("two", "two2two")
-    line = line.replace("three", "three3three")
-    line = line.replace("four", "four4four")
-    line = line.replace("five", "five5five")
-    line = line.replace("six", "six6six")
-    line = line.replace("seven", "seven7seven")
-    line = line.replace("eight", "eight8eight")
-    line = line.replace("nine", "nine9nine")
+    for i in range(len(numbers)):
+        line = line.replace(numbers[i], numbers[i] + str(i+1) + numbers[i])
 
-    a = ""
-    b = ""
-    for c in line:
-        if c.isnumeric(): 
-            a = c
-            break
-    for c in line[::-1]:
-        if c.isnumeric(): 
-            b = c
-            break
-    s = s + int(a+b)
+    q = list(filter(str.isdigit, list(line)))
+    s += int(q[0] + q[-1])
 
 print(s)
